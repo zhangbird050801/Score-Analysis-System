3537d57d849224e8a0c6fbef68ff24283e39675b
zhangbird050801 <2944006086@qq.com>
2025-05-12 20:53:20 +0800

    添加注释

--- src/tools/printHelp.c
+++ src/tools/printHelp.c
@@ -1,11 +1,17 @@
 #include "tools/printHelp.h"
 
+/**
+ * @brief 获取字符串的宽度
+ */
 int get_display_width(const char *str) {
     wchar_t wstr[256];
     mbstowcs(wstr, str, 256); 
     return wcswidth(wstr, wcslen(wstr));
 }
 
+/**
+ * @brief 居中打印字符串
+ */
 void print_centered(const char *str, int column_width) {
     int str_width = get_display_width(str);
     if (str_width >= column_width) {
